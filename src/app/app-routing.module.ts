0d24d26170ea3ec0c4c3864c81cc96d19579f1dd
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2022-07-25 17:08:22 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "primeira-pagina", component: PrimeiraPaginaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
