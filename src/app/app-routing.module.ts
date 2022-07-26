d5ce5367e243f2519521b5a9d646bc7200d35494
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2022-07-26 11:56:20 -0300

    Criando rotas lazy-load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   { path: "primeira-pagina", component: PrimeiraPaginaComponent },
   { path: "segunda-pagina", component: SegundaPaginaComponent },
   { path: "", redirectTo: "primeira-pagina", pathMatch: "full" },
-  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent }
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent },
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) }
 ];
 
 @NgModule({
